be710f5cf2c757ce41b2a1b74ee94da47552d0ce
Tony de Faria <wearesocial@iigethr.com>
2018-01-18 12:33:29 +0000

    first commit

---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module MagensinusCloud
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Custom template engine
+    config.generators do |generator|
+      generator.template_engine :haml
+      generator.helper false
+      generator.assets false
+      generator.stylesheets false
+      generator.javascripts false
+      generator.test_framework nil
+    end
+  end
+end
