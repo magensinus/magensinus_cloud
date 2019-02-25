15f5b2d85c4fb888698fb295ea2dd8dd6dce3535
Tony de Faria <tonydefaria@iigethr.com>
2019-02-25 14:27:29 +0000

    better partner thumbs

--- app/uploaders/partnerships/thumb_uploader.rb
+++ app/uploaders/partnerships/thumb_uploader.rb
@@ -2,6 +2,14 @@
 
 module Partnerships
   class ThumbUploader < ::ThumbUploader
+    # Versions
+    # --------
+    # Nano
+    version :thumb do
+      # process resize_to_fill: [480, 270, gravity = "Center"]
+      process resize_to_fill: [512, 512, gravity = "Center"]
+    end
+
     # Prefix variable
     def prefix
       "partnerships-thumb-"
